c27988b5fd94eb966473db62cd4ff80a5aaf4518
Sara-2-1 <sara.aliebra@gmail.com>
2024-09-26 17:06:00 +0300

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Sarah ali  on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
